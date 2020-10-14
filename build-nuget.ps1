945424b48a2f0f3cf0beecea29276f7f00944315
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-10-14 12:16:44 -0400

    moving files and improving build-nuget.ps1

--- build-nuget.ps1
+++ build-nuget.ps1
@@ -19,23 +19,24 @@ function Remove-Folder{
         }
     }
 }
-
+function Reset-Templates{
+    [cmdletbinding()]
+    param()
+    process{
+        'resetting dotnet new templates' | Write-host
+        push-location C:\Users\sayedha\.templateengine
+        get-childitem -path C:\Users\sayedha\.templateengine -directory | remove-item -recurse
+        &dotnet new --debug:reinit
+        pop-location
+    }
+}
 # get-fullpath function needs to be imported
 $vstempfolderpath = ((join-path -Path $srcDir Content\.vs)|Get-FullPath)
 $outputpath = ((Join-Path $scriptDir nupkg)|Get-FullPath)
 Push-Location $srcDir
 sayedha removefolders -f bin -f obj
 Pop-Location
-# [string[]]$foldersToDelete = ($vstempfolderpath,$outputpath)
-# [string[]]$binfolders = Get-ChildItem $srcDir bin -Recurse -Directory|get-fullpath
-# [string[]]$objfolders = Get-ChildItem $srcDir obj -Recurse -Directory|get-fullpath
 
-#if($binfolders){
-#    $foldersToDelete = $foldersToDelete + $binfolders
-#}
-#if($objfolders){
-#    $foldersToDelete =  $foldersToDelete + $objfolders
-#}
 'Folders to delete: {0}' -f $foldersToDelete | Write-Host
 Remove-Folder -path $foldersToDelete
 
@@ -47,3 +48,10 @@ else{
     'nuspec file not found at {0}' -f $pathtonuspec | Write-Error
 }
 
+$pathtonupkg = ((join-path $scriptDir nupkg/SayedHa.Template.NetCoreTool.nuspec.1.0.0.nupkg)|get-fullpath)
+
+Reset-Templates
+if(test-path $pathtonupkg){
+    'installing template "{0}"' -f $pathtonupkg | write-host
+    &dotnet new --install $pathtonupkg
+}
