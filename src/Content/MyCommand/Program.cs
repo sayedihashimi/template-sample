440ce554f3bc388dfc9d8bd9c422c122f06352bd
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-10-15 14:32:34 -0400

    adding framework param, and showing desc in VS

--- src/Content/MyCommand/Program.cs
+++ src/Content/MyCommand/Program.cs
@@ -3,7 +3,7 @@
 namespace MyCommand {
     public class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Description: DescriptionContent Author: AuthorName");
         }
     }
 }
