9ceb173e097a878a9ab89bf4d6db501d6216ea51
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-10-21 23:14:26 -0400

    updating build script to be generic

--- build-nuget.ps1
+++ build-nuget.ps1
@@ -1,57 +1,48 @@
 $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
-$srcDir = ((Join-Path -path $scriptDir src)|get-fullpath)
+$srcDir = (Join-Path -path $scriptDir src)
 # nuget.exe needs to be on the path or aliased
-function Remove-Folder{
-    [cmdletbinding()]
-    param(
-        [Parameter(ValueFromPipeline=$true)]
-        [string[]]$path
-    )
-    process{
-        if(-not $path -or ($path.Length -le 0)){
-            return
-        }
-        foreach ($p in $path) {
-            if((test-path $p) -and -not ([string]::IsNullOrEmpty($p))){
-                'Deleting folder "{0}"' -f $p | Write-Host
-                Remove-Item -Path $p -recurse -Force
-            }
-        }
-    }
-}
 function Reset-Templates{
     [cmdletbinding()]
-    param()
+    param(
+        [string]$templateEngineUserDir = (join-path -Path $env:USERPROFILE -ChildPath .templateengine)
+    )
     process{
-        'resetting dotnet new templates' | Write-host
-        push-location C:\Users\sayedha\.templateengine
-        get-childitem -path C:\Users\sayedha\.templateengine -directory | remove-item -recurse
+        'resetting dotnet new templates. folder: "{0}"' -f $templateEngineUserDir | Write-host
+        get-childitem -path $templateEngineUserDir -directory | Select-Object -ExpandProperty FullName | remove-item -recurse
         &dotnet new --debug:reinit
-        pop-location
     }
 }
-# get-fullpath function needs to be imported
-$vstempfolderpath = ((join-path -Path $srcDir Content\.vs)|Get-FullPath)
-$outputpath = ((Join-Path $scriptDir nupkg)|Get-FullPath)
-Push-Location $srcDir
-sayedha removefolders -f bin -f obj
-Pop-Location
+function Clean(){
+    [cmdletbinding()]
+    param(
+        [string]$rootFolder = $scriptDir
+    )
+    process{
+        'clean started, rootFolder "{0}"' -f $rootFolder | write-host
+        # delete folders that should not be included in the nuget package
+        Get-ChildItem -path $rootFolder -include bin,obj,nupkg -Recurse -Directory | Select-Object -ExpandProperty FullName | Remove-item -recurse
+    }
+}
 
-'Folders to delete: {0}' -f $foldersToDelete | Write-Host
-Remove-Folder -path $foldersToDelete
 
-$pathtonuspec = ((Join-Path $srcDir SayedHa.Template.NetCoreTool.nuspec)|get-fullpath)
+
+# start script
+Clean
+
+$outputpath = Join-Path $scriptDir nupkg
+$pathtonuspec = Join-Path $srcDir SayedHa.Template.NetCoreTool.nuspec
 if(Test-Path $pathtonuspec){
     nuget pack $pathtonuspec -OutputDirectory $outputpath
 }
 else{
-    'nuspec file not found at {0}' -f $pathtonuspec | Write-Error
+    'ERROR: nuspec file not found at {0}' -f $pathtonuspec | Write-Error
+    return
 }
 
-$pathtonupkg = ((join-path $scriptDir nupkg/SayedHa.Template.NetCoreTool.nuspec.1.0.0.nupkg)|get-fullpath)
+$pathtonupkg = join-path $scriptDir nupkg/SayedHa.Template.NetCoreTool.nuspec.1.0.0.nupkg
 
-Reset-Templates
-if(test-path $pathtonupkg){
-    'installing template "{0}"' -f $pathtonupkg | write-host
+if(test-path $pathtonupkg){   
+    Reset-Templates
+    'installing template with command "dotnet new --install {0}"' -f $pathtonupkg | write-host
     &dotnet new --install $pathtonupkg
 }
