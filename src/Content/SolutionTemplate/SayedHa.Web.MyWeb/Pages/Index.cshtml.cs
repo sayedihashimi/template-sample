70a4bed1103389bab7b0139175955c3e5016b1c6
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2021-03-25 15:10:34 -0400

    adding solution template

---
+++ src/Content/SolutionTemplate/SayedHa.Web.MyWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SayedHa.Web.MyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayedHa.Web.MyWeb.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+            ViewData["message"] = new Sample().GetMessage();
+        }
+    }
+}
---
+++ src/Content/SolutionTemplate/SayedHa.Web.MyWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SayedHa.Web.MyWeb.Pages {
+    public class PrivacyModel : PageModel {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+        }
+    }
+}
