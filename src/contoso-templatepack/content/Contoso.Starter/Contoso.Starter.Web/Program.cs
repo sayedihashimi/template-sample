5c57e396bbc4f60b7da8fe9b3aba2312144bea8a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-11-28 15:33:03 -0500

    adding content for web project and item templates, wip

--- src/contoso-templatepack/content/Contoso.Starter/Contoso.Starter.Web/Program.cs
+++ src/contoso-templatepack/content/Contoso.Starter/Contoso.Starter.Web/Program.cs
@@ -1,3 +1,6 @@
+
+DotNetEnv.Env.Load();
+DotNetEnv.Env.TraversePath().Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
